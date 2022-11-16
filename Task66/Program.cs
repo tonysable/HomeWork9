2e3e8f9e11e5fba43502ba7843479c96d8a59742
Sable <bandidos_150@mail.ru>
2022-11-16 21:28:51 +0300

    Задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,11 @@
+﻿void Recurse(int a, int b, int sum)
+{
+    if(a > b)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от a до b: {sum}");
+        return;
+    }
+    sum = sum + (a++);
+    Recurse(a, b, sum);
+}
+Recurse(4, 8, 0);
